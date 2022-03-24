94f3c7810cc0d93508e39484ad742a853a9435f9
Alexylva <nicholas.roncally@gmail.com>
2022-03-24 15:56:18 -0300

    Fixed

--- mathy.js
+++ mathy.js
@@ -2,25 +2,26 @@ function sumTimesProduct(...arr) {
     return arr.reduce((prev, curr) => prev + curr) * arr.reduce((prev, curr) => prev * curr);
 }
 
-function partition(...arr) {
+
+function partition(transformFn, ...arr) {
     const pow = Math.pow(2, arr.length - 1);
     let ret = [], curr, a = 0;
     for (let i = 0; i < pow; i++) {
-        curr = arr.join('');
+        curr = arr;
         for (let k = 0, a = 1; k < arr.length - 1; k++, a++) {
-            if ((1 << k) & i) { curr = curr.substring(0, a) + ',' + curr.substring(a); a++;}
+            if ((1 << k) & i) { curr = [curr.slice(0,a-1), transformFn(curr.slice(a-1,a+1)), curr.slice(a+1)].flat(); a--;}
         }
-        ret.push(eval(`Array.from([${curr}])`));
+        ret.push(curr);
     }
     return ret;
 }
 
 
 //Bruteforce Search of which partition maximizes sumTimesProduct
-function maximize(...arr) {
-    const arrays = partition(...arr);
+function maximize(transform, ...arr) {
+    const arrays = partition(transform, ...arr);
     let ret = 0, curr, sum, prev = 0;
-
+    
     for (let i = 0; i < arrays.length; i++) {
         curr = arrays[i];
         sum = sumTimesProduct(...curr);
@@ -35,5 +36,6 @@ function maximize(...arr) {
 
 let argnums = [...process.argv.splice(2).map(elem => Number.parseFloat(elem))];
 
-console.log(`Maximum: ${maximize(...argnums)}`);
-//console.log(partition(...argnums).sort((a,b) => a.length - b.length));
+let transform = (arr) => Number.parseFloat(arr.join(''));
+console.log(`Maximum: ${maximize(transform, ...argnums)}`);
+//console.log(partition(transform, ...argnums).sort((a,b) => a.length - b.length));
